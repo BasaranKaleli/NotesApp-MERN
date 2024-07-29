8653d2878170ef0bfbc41492b82a51ccdd33f6fe
Basaran Kaleli <basarannkaleli@gmail.com>
2024-07-29 13:33:23 +0300

    feat: Login screen added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />}></Route>
+      <Route path="/login" exact element={<Login />}></Route>
+      <Route path="/signup" exact element={<SignUp />}></Route>
+    </Routes>
+  </Router>
+);
+
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/dashboard" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
-  );
+  return <div>{routes}</div>;
 };
 
 export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <h4 className="text-2xl mb-7">Login</h4>
+
+            <input type="text" placeholder="Email" className="input-box mb-4" />
+
+            <input
+              type="password"
+              placeholder="Password"
+              className="input-box mb-4"
+            />
+
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Not registered yet?{" "}
+              <Link to="/signup" className="font-medium text-primary underline">
+                Sign Up
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
