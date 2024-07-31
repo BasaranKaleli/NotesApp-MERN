95fa02c30ae3729fe5bda0c97083bad1ad0dd248
Basaran Kaleli <basarannkaleli@gmail.com>
2024-07-31 15:16:10 +0300

    feat: Backend node express.js project setup

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// JSON middleware'ini doğru şekilde ekleyin
 app.use(express.json());
 
-// CORS middleware'ini ayarlayın
 app.use(
   cors({
     origin: "*",
   })
 );
 
-// Basit bir GET isteği ile test endpoint'i oluşturun
 app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
 
-// Sunucuyu başlatın ve port numarasını belirleyin
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Uygulamayı dışa aktarın
 module.exports = app;
+
+//tesst
